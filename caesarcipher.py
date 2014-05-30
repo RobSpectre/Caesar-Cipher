3110a34b86af3e8999433089763e02160ede0879
Rob Spectre <rob.spectre@gmail.com>
2014-05-30 18:32:51 -0500

    1) Added support for cracking Caesar ciphertext. 2) Added tests for crackign Caesar ciphertext.

--- caesarcipher.py
+++ caesarcipher.py
@@ -2,11 +2,12 @@ import argparse
 import logging
 from random import randrange
 import string
+import math
 
 
 class CaesarCipher(object):
     def __init__(self, message=None, encode=False, decode=False, offset=False,
-                 alphabet=None):
+                 crack=None, verbose=None, alphabet=None):
         """
         A class that encodes and decodes strings using the Caesar shift cipher.
 
@@ -25,6 +26,10 @@ class CaesarCipher(object):
                 command line script flag.
             decoded: A boolean indicating desire to decode the string, used as
                 command line script flag.
+            cracked: A boolean indicating to desire to crack the string, used
+                as command line script flag.
+            verbose: A boolean indicating the desire to turn on debug output,
+                use as command line script flag.
             offset: Integer by which you want to shift the value of a letter.
             alphabet: A tuple containing the ASCII alphabet in uppercase.
         """
@@ -32,11 +37,43 @@ class CaesarCipher(object):
         self.encode = encode
         self.decode = decode
         self.offset = offset
+        self.verbose = verbose
+        self.crack = crack
         self.alphabet = alphabet
 
-        # Get alphabet based on locale value set on machine.
+        # Frequency of letters used in English, taken from Wikipedia.
+        # http://en.wikipedia.org/wiki/Letter_frequency
+        self.frequency = {
+            'a': 0.08167,
+            'b': 0.01492,
+            'c': 0.02782,
+            'd': 0.04253,
+            'e': 0.130001,
+            'f': 0.02228,
+            'g': 0.02015,
+            'h': 0.06094,
+            'i': 0.06966,
+            'j': 0.00153,
+            'k': 0.00772,
+            'l': 0.04025,
+            'm': 0.02406,
+            'n': 0.06749,
+            'o': 0.07507,
+            'p': 0.01929,
+            'q': 0.00095,
+            'r': 0.05987,
+            's': 0.06327,
+            't': 0.09056,
+            'u': 0.02758,
+            'v': 0.00978,
+            'w': 0.02360,
+            'x': 0.00150,
+            'y': 0.01974,
+            'z': 0.00074}
+
+        # Get ASCII alphabet if one is not provided by the user.
         if alphabet is None:
-            self.alphabet = {} 
+            self.alphabet = {}
             self.alphabet['lower'] = tuple(string.ascii_lowercase)
             self.alphabet['upper'] = tuple(string.ascii_uppercase)
 
@@ -48,7 +85,6 @@ class CaesarCipher(object):
 
         Required attributes:
             message
-            offset
 
         Returns:
             String with cipher applied.
@@ -61,14 +97,15 @@ class CaesarCipher(object):
         logging.debug("Offset set: {0}".format(self.offset))
 
         # Cipher
-        logging.info("Encoding message: {0}".format(self.message))
         ciphered_message_list = list(self.message)
         for i, letter in enumerate(ciphered_message_list):
             if letter.isalpha():
                 if letter.isupper():
-                    alphabet = self.alphabet['upper']
+                    alphabet = self.alphabet.get('upper', None)
                 else:
-                    alphabet = self.alphabet['lower']
+                    alphabet = self.alphabet.get('lower', None)
+                if alphabet is None:
+                    alphabet = self.alphabet
 
                 logging.debug("Letter: {0}".format(letter))
                 value = alphabet.index(letter)
@@ -81,6 +118,58 @@ class CaesarCipher(object):
         self.message = ''.join(ciphered_message_list)
         return self.message
 
+    def calculate_entropy(self, entropy_string):
+        """Calculates the entropy of a string based on known frequency of
+        English letters.
+
+        Args:
+            entropy_string: A str representing the string to calculate.
+
+        Returns:
+            A negative float with the total entropy of the string (higher
+            is better).
+        """
+        total = 0
+        ignored_chars = []
+        for char in entropy_string:
+            if char.isalpha():
+                total += math.log(self.frequency[char.lower()])
+            else:
+                ignored_chars.append(char)
+        logging.debug("Total sum of freq logging. of letters: {0}".format(
+                      total))
+        logging.debug("Total ignored characters: {0}".format(ignored_chars))
+        return total / math.log(2) / len(ignored_chars)
+
+    @property
+    def cracked(self):
+        """Attempts to crack ciphertext using frequency of letters in English.
+
+        Returns:
+            String of most likely message.
+        """
+        logging.info("Cracking message: {0}".format(self.message))
+        entropy_values = {}
+        message = self.message
+        for i in range(25):
+            self.message = message
+            self.offset = i * -1
+            logging.debug("Attempting crack with offset: "
+                          "{0}".format(self.offset))
+            test_cipher = self.cipher()
+            logging.debug("Attempting plaintext: {0}".format(test_cipher))
+            entropy_values[i] = self.calculate_entropy(test_cipher)
+
+        sorted_by_entropy = sorted(entropy_values, key=entropy_values.get,
+                                   reverse=True)
+        self.offset = sorted_by_entropy[0] * -1
+        self.message = message
+        cracked_text = self.cipher()
+
+        logging.debug("Most likely offset: {0}".format(self.offset))
+
+        return cracked_text
+
     @property
     def encoded(self):
         """Encodes message using Caesar shift cipher
@@ -88,6 +177,7 @@ class CaesarCipher(object):
         Returns:
             String encoded with cipher.
         """
+        logging.info("Encoding message: {0}".format(self.message))
         return self.cipher()
 
     @property
@@ -100,29 +190,34 @@ class CaesarCipher(object):
         Returns:
             String decoded with cipher.
         """
+        logging.info("Decoding message: {0}".format(self.message))
         self.offset = self.offset * -1
         return self.cipher()
 
 
 class CaesarCipherError(Exception):
     def __init__(self, message):
-        logging.error(message)
+        logging.error("ERROR: {0}".format(message))
+        logging.error("Try running with --help for more information.")
 
-# Logging configuration
-logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 # Parser configuration
-parser = argparse.ArgumentParser(description="Caesar Cipher - encode and "
-                                             "decode messages with an English"
-                                             "alphabet offset.",
+parser = argparse.ArgumentParser(description="Caesar Cipher - encode, decode "
+                                             "or crack messages with an "
+                                             "English alphabet offset.",
                                  epilog="Written by Rob Spectre for Hacker "
-                                        "Olympics London.")
+                                 "Olympics London.\n"
+                                 "http://www.brooklynhacker.com")
 parser.add_argument('message',
-                    help="Message to be encoded or decoded.")
+                    help="Message to be encoded, decoded or cracked.")
 parser.add_argument('-e', '--encode', action="store_true",
                     help="Encode this message.")
 parser.add_argument('-d', '--decode', action="store_true",
                     help="Decode this message.")
+parser.add_argument('-c', '--crack', action="store_true",
+                    help="Crack this ciphertext to find most likely message.")
+parser.add_argument('-v', '--verbose', action="store_true",
+                    help="Turn on verbose output.")
 parser.add_argument('-o', '--offset',
                     help="Integer offset to encode/decode message against.")
 parser.add_argument('-a', '--alphabet',
@@ -132,6 +227,18 @@ parser.add_argument('-a', '--alphabet',
 if __name__ == "__main__":
     caesar_cipher = CaesarCipher()
     parser.parse_args(namespace=caesar_cipher)
+
+    # Logging configuration
+    if caesar_cipher.verbose is True:
+        log_level = logging.DEBUG
+        log_format = "%(asctime)s - %(levelname)s: %(message)s"
+    else:
+        log_level = logging.INFO
+        log_format = "%(message)s"
+
+    logging.basicConfig(level=log_level, format=log_format)
+
+    # Non-required arguments and error conditions.
     if caesar_cipher.offset:
         caesar_cipher.offset = int(caesar_cipher.offset)
     if caesar_cipher.offset is False and caesar_cipher.decode is True:
@@ -139,9 +246,16 @@ if __name__ == "__main__":
                                 "selecting an offset.  Please try "
                                 "again with -o switch.")
     if caesar_cipher.encode is True and caesar_cipher.decode is True:
-        raise CaesarCipherError("Please select to encode *or* decode message, "
+        raise CaesarCipherError("Please select to encode or encode a message, "
                                 "not both.")
-    if caesar_cipher.encode is True:
-        print("Encoded message: {0}".format(caesar_cipher.encoded))
+
+    # Required arguments.
     if caesar_cipher.decode is True:
-        print("Decoded message: {0}".format(caesar_cipher.decoded))
+        logging.info("Decoded message: {0}".format(caesar_cipher.decoded))
+    elif caesar_cipher.crack is True:
+        logging.info("Cracked message: {0}".format(caesar_cipher.cracked))
+    elif caesar_cipher.encode is True:
+        logging.info("Encoded message: {0}".format(caesar_cipher.encoded))
+    else:
+        logging.error("Please select a message to encode, decode or "
+                      "crack.  For more information, use --help.")
--- test_caesarcipher.py
+++ test_caesarcipher.py
@@ -79,3 +79,149 @@ class CaesarCipherRegressionTest(unittest.TestCase):
             test_cipher = CaesarCipher(message, encode=True, offset=i)
             test_cipher.encoded
             self.assertEquals(message, test_cipher.decoded)
+
+
+class CaesarCipherCrackTest(unittest.TestCase):
+    def test_calculate_entropy_zero_offset(self):
+        message = "The quick brown fox jumps over the lazy dog."
+        test_cipher = CaesarCipher(message, crack=True)
+        confirmed_entropy_value = -19.904685894478845
+        test_entropy_value = test_cipher.calculate_entropy(message)
+        self.assertEquals(confirmed_entropy_value, test_entropy_value)
+
+    def test_crack(self):
+        ciphertext = "Rfc osgai zpmul dmv hsknq mtcp rfc jyxw bme."
+        plaintext = "The quick brown fox jumps over the lazy dog."
+        test_crack = CaesarCipher(ciphertext, crack=True)
+        self.assertEquals(plaintext, test_crack.cracked)
+
+
+class CaesarCipherCrackRegressionTest(unittest.TestCase):
+    def test_lots_of_cracks(self):
+        plaintexts = [
+            "London calling to the faraway towns",
+            "Now war is declared and battle come down",
+            "London calling to the underworld",
+            "Come out of the cupboard, you boys and girls",
+            "London calling, now don't look to us",
+            "Phony Beatlemania has bitten the dust",
+            "London calling, see we ain't got no swing",
+            "'Cept for the ring of that truncheon thing",
+            "The ice age is coming, the sun is zooming in",
+            "Meltdown expected, the wheat is growin' thin",
+            "Engines stop running, but I have no fear",
+            "Cause London is drowning, and I, I live by the river",
+            "London calling to the imitation zone",
+            "Forget it, brother, you can go it alone",
+            "London calling to the zombies of death",
+            "Quit holding out and draw another breath",
+            "London calling and I don't want to shout",
+            "But when we were talking I saw you nodding out",
+            "London calling, see we ain't got no high",
+            "Except for that one with the yellowy eye",
+            "Now get this",
+            "London calling, yes, I was there, too",
+            "An' you know what they said? Well, some of it was true!",
+            "London calling at the top of the dial",
+            "And after all this, won't you give me a smile?",
+            "I never felt so much a' like a'like a'like",
+            "When they kick at your front door",
+            "How you gonna come?",
+            "With your hands on your head",
+            "Or on the trigger of your gun",
+            "When the law break in",
+            "How you gonna go?",
+            "Shot down on the pavement",
+            "Or waiting on death row",
+            "You can crush us",
+            "You can bruise us",
+            "But you'll have to answer to",
+            "Oh, the guns of Brixton",
+            "The money feels good",
+            "And your life you like it well",
+            "But surely your time will come",
+            "As in heaven, as in hell",
+            "You see, he feels like Ivan",
+            "Born under the Brixton sun",
+            "His game is called survivin'",
+            "At the end of the harder they come",
+            "You know it means no mercy",
+            "They caught him with a gun",
+            "No need for the Black Maria",
+            "Goodbye to the Brixton sun",
+            "You can crush us",
+            "You can bruise us",
+            "Yes, even shoot us",
+            "But oh-the guns of Brixton",
+            "Shot down on the pavement",
+            "Waiting in death row",
+            "His game is called survivin'",
+            "As in heaven as in hell",
+            "Anybody who makes speeches written ",
+            "by someone else is just a robot."]
+
+        ciphertexts = [
+            "Cfeufe trcczex kf kyv wrirnrp kfnej",
+            "Tuc cgx oy jkirgxkj gtj hgzzrk iusk juct",
+            "Twvlwv kittqvo bw bpm cvlmzewztl",
+            "Lxvn xdc xo cqn ldykxjam, hxd kxhb jwm praub",
+            "Bedted sqbbydw, dem ted'j beea je ki",
+            "Yqxwh Knjcunvjwrj qjb krccnw cqn mdbc",
+            "Hkjzkj ywhhejc, oaa sa wej'p ckp jk osejc",
+            "'Lnyc oxa cqn arwp xo cqjc cadwlqnxw cqrwp",
+            "Lzw auw syw ak ugeafy, lzw kmf ak rggeafy af",
+            "Rjqyitbs jcujhyji, ymj bmjfy nx lwtbns' ymns",
+            "Oxqsxoc cdyz bexxsxq, led S rkfo xy pokb",
+            "Usmkw Dgfvgf ak vjgofafy, sfv A, A danw tq lzw janwj",
+            "Cfeufe trcczex kf kyv zdzkrkzfe qfev",
+            "Oxapnc rc, kaxcqna, hxd ljw px rc juxwn",
+            "Twvlwv kittqvo bw bpm hwujqma wn lmibp",
+            "Mqep dkhzejc kqp wjz znws wjkpdan xnawpd",
+            "Gjiyji xvggdib viy D yji'o rvio oj ncjpo",
+            "Mfe hspy hp hpcp elwvtyr T dlh jzf yzootyr zfe",
+            "Jmlbml ayjjgle, qcc uc ygl'r emr lm fgef",
+            "Votvgk wfi kyrk fev nzky kyv pvccfnp vpv",
+            "Stb ljy ymnx",
+            "Ehgwhg vteebgz, rxl, B ptl maxkx, mhh",
+            "Iv' gwc svwe epib bpmg aiql? Emtt, awum wn qb eia bzcm!",
+            "Svukvu jhsspun ha aol avw vm aol kphs",
+            "Reu rwkvi rcc kyzj, nfe'k pfl xzmv dv r jdzcv?",
+            "E jaran bahp ok iqyd w' hega w'hega w'hega",
+            "Lwtc iwtn zxrz pi ndjg ugdci sddg",
+            "Yfn pfl xfeer tfdv?",
+            "Lxiw ndjg wpcsh dc ndjg wtps",
+            "Il ih nby nlcaayl iz siol aoh",
+            "Bmjs ymj qfb gwjfp ns",
+            "Mtb dtz ltssf lt?",
+            "Hwdi sdlc dc iwt epktbtci",
+            "Tw bfnynsl ts ijfym wtb",
+            "Qgm usf ujmkz mk",
+            "Gwc kiv jzcqam ca",
+            "Jcb gwc'tt pidm bw ivaemz bw",
+            "Mf, rfc eslq md Zpgvrml",
+            "Kyv dfevp wvvcj xffu",
+            "Wjz ukqn heba ukq hega ep sahh",
+            "Rkj ikhubo oekh jycu mybb secu",
+            "Xp fk ebxsbk, xp fk ebii",
+            "Hxd bnn, qn onnub urtn Rejw",
+            "Uhkg ngwxk max Ukbqmhg lng",
+            "Opz nhtl pz jhsslk zbycpcpu'",
+            "Fy ymj jsi tk ymj mfwijw ymjd htrj",
+            "Fvb ruvd pa tlhuz uv tlyjf",
+            "Znke igamnz nos cozn g mat",
+            "Yz yppo qzc esp Mwlnv Xlctl",
+            "Zhhwurx mh max Ukbqmhg lng",
+            "Vlr zxk zorpe rp",
+            "Oek sqd rhkyiu ki",
+            "Lrf, rira fubbg hf",
+            "Kdc xq-cqn pdwb xo Kargcxw",
+            "Dsze ozhy zy esp algpxpye",
+            "Osalafy af vwslz jgo",
+            "Efp dxjb fp zxiiba prosfsfk'",
+            "Rj ze yvrmve rj ze yvcc",
+            "Ylwzmbw ufm kyicq qnccafcq upgrrcl ",
+            "ur lhfxhgx xelx bl cnlm t khuhm."]
+
+        for i, ciphertext in enumerate(ciphertexts):
+            test_cipher = CaesarCipher(ciphertext, crack=True)
+            self.assertEquals(plaintexts[i], test_cipher.cracked)
